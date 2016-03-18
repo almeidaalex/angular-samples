5da183dcdc2207831607b5cd983a4a6010fbab73
Alex Almeida <alalmeida@bvmf.com.br>
2016-03-17 22:56:45 -0300

    several improvements

--- unit-test-angular/app/columns.drv.js
+++ unit-test-angular/app/columns.drv.js
@@ -1,52 +1,68 @@
 (function(){
   'use strict';
 
-  angular.module('adn.tables',[])
-  .directive('columnsSelector', ColumnsSelector)
-  .directive('btnColumnsSelector', BtnColumnsSelector);
+  angular.module('adn.tables',['ngCookies'])
+  .directive('columnsSelector', ['columnStateService',ColumnsSelector])
+  .directive('btnColumnsSelector', BtnColumnsSelector)
+  .factory('columnStateService', ['$cookies',ColumnStateService]);
 
-  function ColumnsSelector() {
+  function ColumnsSelector(columnStateService) {
     return {
       restrict: 'A',
       scope: true,
-      controller: ['$scope',function($scope) {        
+      controller: ['$scope',function($scope) {
+                
         $scope.columns = [];
         $scope.tableElement;
         
         $scope.toggle = function(event, column) {
           column.isVisible = !column.isVisible;
-          var header = $scope.tableElement.find('th#'+ column.id);
-          var associatedColumns = header.closest('table').find('td[headers='+ column.id +']');
-          
-          if (column.isVisible) {
-            header.show();
-            associatedColumns.show();
-          }
-          else {
-            header.hide();
-            associatedColumns.hide();            
-          }
+          if (column.isVisible) 
+            $scope.showColumn(column)          
+          else                 
+            $scope.hideColumn(column);
+            
+          columnStateService.save($scope.columns);
+        }
+        
+        $scope.hideColumn = function(column) {
+          var elements = getColumnElements(column);
+          elements.header.hide(); 
+          elements.associatedColumns.hide();
+        }
+        
+        $scope.showColumn = function (column) {
+          var elements = getColumnElements(column);
+          elements.header.show();
+          elements.associatedColumns.show();  
+        }
+        
+        function getColumnElements(column){
+            var _header = $scope.tableElement.find('th#'+ column.id);
+            var _associatedColumns = _header.closest('table').find('td[headers='+ column.id +']');
+            return {
+                header : _header,
+                associatedColumns: _associatedColumns  
+            }  
         }
       }],
       compile: function(element, attr) {
         return {
-          pre:  function(scope, element, attr){
+          pre:  function(scope, element, attr){              
+            columnStateService.setup({key: element.data('selector-key')});   
+                     
             scope.tableElement = element.find('table'); 
-            
-            scope.$watchCollection(attr.columnsSelector, function (newValue) {
-                if (angular.isDefined(newValue)){
-                    angular.forEach(scope.columns, function(column){
-                       column.isVisible = false;
-                       var c = findColumn(newValue, column.id);
-                       angular.merge(column, c);   
-                    });                   
-                }
-            });
             
             var columns =  scope.tableElement.find('th');
             angular.forEach(columns, function(column, idx){
               scope.columns.push({index: idx, name: angular.element(column).text(), isVisible: true, id: column.id});
-            });            
+            });           
+           
+            var defaultAttributesWatch = scope.$watchCollection(attr.columnsSelector, function (newValue) {                
+                loadFromDefaultState(newValue, scope);                
+            });
+            
+            loadFromState(defaultAttributesWatch, scope);         
           }
         }
         
@@ -56,10 +72,36 @@
                     return columns[i];     
             }
         }
+        
+        function loadFromState(watchDefaultState, scope){
+            var lastConfiguration = columnStateService.load(); 
+            if (lastConfiguration) {            
+                watchDefaultState();
+                
+                angular.forEach(lastConfiguration, function(column){ 
+                       if(column.isVisible)   
+                         scope.showColumn(column);
+                       else
+                         scope.hideColumn(column);
+                }); 
+            }
+        }
+        
+        function loadFromDefaultState(newValue, scope) {
+            if (angular.isDefined(newValue)){
+                angular.forEach(scope.columns, function(column){
+                    column.isVisible = false;
+                    scope.hideColumn(column);
+                    var c = findColumn(newValue, column.id);
+                    angular.merge(column, c);                       
+                    if(column.isVisible)   
+                        scope.showColumn(column);
+                });                   
+            }
+        }
       }
     }
   }  
-    
   
   function BtnColumnsSelector() {
     return {
@@ -80,5 +122,22 @@
                 '</a>'+
                 '</li>';
     }
+  }
+  
+  function ColumnStateService($cookies){
+      var _stateKey;
+      return {
+          setup: function(info){
+              _stateKey = info.key;
+          },
+          save: function(config){
+              if (angular.isUndefined(_stateKey) || _stateKey === '')
+                throw Error('The StateKey was not configurated, please, run setup before');
+              $cookies.put(_stateKey, config);
+          },          
+          load: function(key){
+              return $cookies.get(_stateKey);
+          }
+      }
   }
 })();
--- unit-test-angular/spec/columns.spec.js
+++ unit-test-angular/spec/columns.spec.js
@@ -1,14 +1,28 @@
 'use strict';
 
 describe('A diretiva de seleção de coluna', function(){
-  var $compile, scope, tableTemplate;
+  var $compile, scope, tableTemplate, columnStateService;
 
-  beforeEach(module('adn.tables'));
-
-  beforeEach(inject(function(_$compile_, _$rootScope_){
+  beforeEach(module('adn.tables')); 
+  
+  beforeEach(function () {
+        angular.module('ngCookies',[]);
+      
+        module(function ($provide) {
+            $provide.service('columnStateService', function () {
+                this.save = jasmine.createSpy('key');
+                this.load = jasmine.createSpy('load');
+                this.setup = jasmine.createSpy('setup');
+            });
+        });
+  });
+  
+  
+  beforeEach(inject(function(_$compile_, _$rootScope_, _columnStateService_){
     $compile = _$compile_;
     scope = _$rootScope_.$new();
-    tableTemplate = '<div columns-selector="">' +
+    columnStateService = _columnStateService_;
+    tableTemplate = '<div columns-selector="" data-selector-key="test-key">' +
                         '<table>' +
                             '<thead><tr>'+
                                 '<th id="c1">Column1</th>' +
@@ -19,7 +33,7 @@ describe('A diretiva de seleção de coluna', function(){
                             '</tr></thead>' + 
                          '</table>' +
                      '</div>';
-  }));
+  }));  
 
   it('deverá ler todos os headers da tabela e criar seus respectivos objetos', function(){
      var element = $compile('<div columns-selector=""><table><thead><tr><th id="c1">Column1</th><th id="c2">Column2</th></tr></thead></table></div>')(scope);
@@ -62,7 +76,7 @@ describe('A diretiva de seleção de coluna', function(){
   
    it('quando um valor de coluna padrão for definido, as demais colunas devem ficarem ocultas', function(){
       scope.defaultColumns = [
-            {id:"c1"},
+            {id:"c1", isVisible: true},
             {id:"c3", isVisible: false},
         ];
         
@@ -74,12 +88,46 @@ describe('A diretiva de seleção de coluna', function(){
      
      expect(sc.columns.length).toEqual(5);
      
+     expect(angular.element(element.html()).find('th#c1').css('display')).toEqual('table-cell');
      expect(angular.element(element.html()).find('th#c3').css('display')).toEqual('none');
      expect(angular.element(element.html()).find('th#c2').css('display')).toEqual('none');
      expect(angular.element(element.html()).find('th#c4').css('display')).toEqual('none');
      expect(angular.element(element.html()).find('th#c5').css('display')).toEqual('none');
   })
   
+   it('deverá armazenar a última configuração de colunas do usuário', function(){
+    columnStateService.load = function () {
+        return [
+                {id: 'c1', isVisible: true},
+                {id: 'c2', isVisible: false},
+                {id: 'c3', isVisible: true}, 
+                {id: 'c4', isVisible: false},                        
+                {id: 'c5', isVisible: true}
+            ];
+    };
+    columnStateService.save = jasmine.createSpy('saved');
+        
+     var element = $compile(tableTemplate)(scope);
+     scope.$digest();
+     var sc = element.scope();
+     
+     expect(angular.element(element.html()).find('th#c1').css('display')).toEqual('table-cell');     
+     expect(angular.element(element.html()).find('th#c2').css('display')).toEqual('none');
+     expect(angular.element(element.html()).find('th#c3').css('display')).toEqual('table-cell');
+     expect(angular.element(element.html()).find('th#c4').css('display')).toEqual('none');
+     expect(angular.element(element.html()).find('th#c5').css('display')).toEqual('table-cell');
+  });
+  
+  it('deverá salvar o estado em cada alteração de visibiliade da coluna', function(){
+     columnStateService.save = jasmine.createSpy('saved');
+     var element = $compile(tableTemplate)(scope);
+     scope.$digest();
+     var sc = element.scope();
+     
+     sc.toggle({}, sc.columns[0]);
+     
+     expect(columnStateService.save).toHaveBeenCalled();
+  });
   
   afterEach(function(){
       scope.$destroy();
